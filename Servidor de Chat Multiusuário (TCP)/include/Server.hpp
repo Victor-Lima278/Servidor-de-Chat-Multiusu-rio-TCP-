14ac8186f0b2f2efc2edd18887f4a36036403ba0
Victor Cavalcante Santos Lima <victor.vcsl50@gmail.com>
2025-10-03 17:14:00 -0300

    Update Server.hpp

--- Servidor de Chat Multiusuário (TCP)/include/Server.hpp
+++ Servidor de Chat Multiusuário (TCP)/include/Server.hpp
@@ -1,10 +1,30 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <string>
+
 class Server {
 public:
-    void start(int port);
+    Server(int port);
+    ~Server();
+
+    void start();
     void stop();
+
+private:
+    int server_fd;
+    int port;
+    bool running;
+
+    std::vector<int> clients;
+    std::mutex clients_mtx;
+
+    void acceptClients();
+    void handleClient(int client_socket);
+    void broadcast(const std::string& msg, int sender_fd);
 };
 
 #endif // SERVER_HPP
